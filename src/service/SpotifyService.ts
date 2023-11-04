68d5177c22db08b22913105b8c994eddaaa5a55c
ravi <raviabeypaul@gmail.com>
2023-11-04 18:57:24 -0400

    cleanup

--- src/service/SpotifyService.ts
+++ src/service/SpotifyService.ts
@@ -29,17 +29,4 @@ export class SpotifyService {
         return res.body
     }
 
-
-    getTrack = async () => {
-    }
-
-    generateRandomString =  (length : number)=> {
-        var text = '';
-        var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-      
-        for (var i = 0; i < length; i++) {
-          text += possible.charAt(Math.floor(Math.random() * possible.length));
-        }
-        return text;
-      };
 }
